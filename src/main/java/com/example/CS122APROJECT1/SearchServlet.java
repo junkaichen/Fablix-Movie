954208081e85be1bdd138e3e86a40ac5cf68e097
JunKai Chen <junkaichenalex@gmail.com>
2021-04-22 23:35:43 -0700

    Search Function

--- src/main/java/com/example/CS122APROJECT1/SearchServlet.java
+++ src/main/java/com/example/CS122APROJECT1/SearchServlet.java
@@ -85,6 +85,7 @@
 
 
 package com.example.CS122APROJECT1;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
@@ -102,18 +103,41 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 
 @WebServlet(name = "SearchServlet", urlPatterns = "/api/search")
 public class SearchServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 2L;
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     // Create a dataSource which registered in web.xml
+    // Create a dataSource which registered in web.
     private DataSource dataSource;
 
+
+    public String handleQuery(ArrayList<String> user_inputs, ArrayList<String> user_inputsTypes)
+    {
+        String outputQuery = "";
+        if(user_inputs.size() == 1)
+        {
+            if (user_inputsTypes.get(0).equals("TITLE"))
+            {
+                outputQuery = String.format("SELECT * from movies M, ratings R, genres_in_movies I, genres G, stars_in_movies S, " +
+                        "stars T WHERE M.id = R.movieId AND R.movieId = I.movieId AND I.genreId = G.id AND R.movieId = S.movieId " +
+                        "AND S.starId = T.id AND M.title like '%s'", user_inputs.get(0));
+            }
+            else if (user_inputsTypes.get(0).equals("YEAR"))
+            {
+
+                outputQuery = "SELECT * from movies M, ratings R, genres_in_movies I, genres G, stars_in_movies S, " +
+                        "stars T WHERE M.id = R.movieId AND R.movieId = I.movieId AND I.genreId = G.id AND R.movieId = S.movieId " +
+                        "AND S.starId = T.id AND M.year =" + "2003";
+//                outputQuery += "ORDER BY R.rating  DESC";
+            }
+        }
+
+        return outputQuery;
+    }
     public void init(ServletConfig config) {
         try {
             dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
@@ -122,59 +146,194 @@ public class SearchServlet extends HttpServlet {
         }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
 
         try {
-            // Create a new connection to database
-            Connection dbCon = dataSource.getConnection();
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+
+            // Declare our statement
+            Statement statement = dbcon.createStatement();
+            ArrayList <String> user_inputs = new ArrayList<String>();
+            ArrayList <String> user_inputsTypes = new ArrayList<String>();
+            String input_title = request.getParameter("title");
+            String input_year = request.getParameter("year");
+            String input_director= request.getParameter("director");
+            String input_star = request.getParameter("star");
+
+            Integer querytype = -1;
+            if(input_title != ""){user_inputs.add(input_title); user_inputsTypes.add("TITLE"); querytype = 0;}
+            if(input_year != ""){user_inputs.add(input_year); user_inputsTypes.add("YEAR"); querytype = 1;}
+            if(input_director != ""){user_inputs.add(input_director); user_inputsTypes.add("DIRECTOR");}
+            if(input_star != ""){user_inputs.add(input_star); user_inputsTypes.add("STAR");}
+
+            String query = handleQuery(user_inputs, user_inputsTypes);
 
 
-            // Declare a new statement
-            Statement statement = dbCon.createStatement();
-
-
-            /* get the content in the login box from the http request(user's typed in username and the password)
-                which refers to the value of <input name="name"> in index.html*/
-            String movietitle = request.getParameter("movietitle");
-
-            JsonObject responseJsonObject = new JsonObject();
-
-            // Generate a SQL query
-            String query = String.format("SELECT * from movies where title like '%s'", movietitle);
-
-            // Perform the query
             ResultSet rs = statement.executeQuery(query);
 
-//            System.out.println(movie_title);
-            // Check the Username exists in database or not
-            if(rs.next() == false)
+            if(querytype == 0)
             {
-                // the Username doesn't exists in database
-                responseJsonObject.addProperty("status", "fail");
-                responseJsonObject.addProperty("message", "user " + movietitle+ " doesn't exist");
-            }
-            // the Username exists in database
-            else
-            {
+                JsonObject jsonObject = new JsonObject();
+                JsonArray jsonArray = new JsonArray();
+                int countGenres = 0;
+                int countStars = 0;
+                // Perform the query
+                if(rs.next()) {
                     String movie_title = rs.getString("title");
-                    request.getSession().setAttribute("user", new User(movietitle));
-                    responseJsonObject.addProperty("status", "success");
-                    responseJsonObject.addProperty("message", "success");
-
+                    String movie_id = rs.getString("id");
+                    String movie_year = rs.getString("year");
+                    String movie_director = rs.getString("director");
+                    String movie_rating = rs.getString("rating");
+                    String movie_nameOfGenres = rs.getString("name");
+                    String movie_star = rs.getString("starname");
+                    String movie_starid = rs.getString("starId");
+                    JsonArray stars_array = new JsonArray();
+                    JsonArray starsId_array = new JsonArray();
+                    stars_array.add(movie_star);
+                    starsId_array.add(movie_starid);
+                    jsonObject.addProperty("movie_title", movie_title);
+                    jsonObject.addProperty("movie_id", movie_id);
+                    jsonObject.addProperty("movie_director", movie_director);
+                    jsonObject.addProperty("movie_year", movie_year);
+                    jsonObject.addProperty("movie_rating", movie_rating);
+                    while(rs.next())
+                    {
+                        movie_star = rs.getString("starname");
+                        movie_starid = rs.getString("starId");
+                        String movie_nameOfGenres2 = rs.getString("name");
+                        if(countGenres < 2 && !movie_nameOfGenres.contains(movie_nameOfGenres2))
+                        {
+                            movie_nameOfGenres = movie_nameOfGenres + ", " + movie_nameOfGenres2;
+                            countGenres++;
+                        }
+                        if(countStars < 3)
+                        {
+                            stars_array.add(movie_star);
+                            starsId_array.add(movie_starid);
+                            countStars++;
+                        }
+                    }
+                    jsonObject.add("movie_starid", starsId_array);
+                    jsonObject.addProperty("searchType", "TITLE");
+                    jsonObject.add("movie_star", stars_array);
+                    jsonObject.addProperty("movie_nameOfGenres", movie_nameOfGenres);
+                    jsonArray.add(jsonObject);
+                }
+                // write JSON string to output
+                out.write(jsonArray.toString());
                 // set response status to 200 (OK)
                 response.setStatus(200);
+            }
+            else if(querytype == 1)
+            {
 
-                rs.close();
-                statement.close();
-                dbCon.close();
+                JsonArray jsonArray = new JsonArray();
+                int countGenres = 0;
+                int countStars = 0;
+                // Perform the query
+
+                JsonArray stars_array = new JsonArray();
+                JsonArray starsId_array = new JsonArray();
+                rs.next();
+                String movie_title = rs.getString("title");
+                String movie_id = rs.getString("id");
+                String movie_year = rs.getString("year");
+                String movie_director = rs.getString("director");
+                String movie_nameOfGenres = rs.getString("name");
+                String movie_star = rs.getString("starname");
+                String movie_rating = rs.getString("rating");
+                String movie_starid = rs.getString("starId");
+                stars_array.add(movie_star);
+                starsId_array.add(movie_starid);
+                rs.next();
+                do {
+                    String movie_title2 = rs.getString("title");
+                    String movie_id2 = rs.getString("id");
+                    String movie_year2 = rs.getString("year");
+                    String movie_director2 = rs.getString("director");
+                    String movie_nameOfGenres2 = rs.getString("name");
+                    String movie_star2 = rs.getString("starname");
+                    String movie_rating2 = rs.getString("rating");
+                    String movie_starid2 = rs.getString("starId");
+                    if(!movie_id.equals(movie_id2))
+                    {
+                        JsonObject jsonObject = new JsonObject();
+                        jsonObject.addProperty("searchType", "YEAR");
+                        jsonObject.addProperty("movie_title", movie_title);
+                        jsonObject.addProperty("movie_id", movie_id);
+                        jsonObject.addProperty("movie_director", movie_director);
+                        jsonObject.addProperty("movie_nameOfGenres", movie_nameOfGenres);
+                        jsonObject.addProperty("movie_year", movie_year);
+                        jsonObject.addProperty("movie_rating", movie_rating);
+                        jsonObject.add("movie_starid", starsId_array);
+                        jsonObject.add("movie_star", stars_array);
+                        jsonArray.add(jsonObject);
+                        movie_id = movie_id2;
+                        movie_title = movie_title2;
+                        movie_year = movie_year2;
+                        movie_director = movie_director2;
+                        movie_rating = movie_rating2;
+                        movie_nameOfGenres = movie_nameOfGenres2;
+                        stars_array = new JsonArray();
+                        starsId_array = new JsonArray();
+                        countStars = 0;
+                        countGenres = 0;
+                    }
+                    else {
+                        movie_id = movie_id2;
+                        movie_title = movie_title2;
+                        movie_year = movie_year2;
+                        movie_director = movie_director2;
+                        movie_rating = movie_rating2;
+
+
+                        if(countGenres < 3 && !movie_nameOfGenres.contains(movie_nameOfGenres2)){
+                            movie_nameOfGenres = movie_nameOfGenres + ", " + movie_nameOfGenres2;
+                            countGenres++;
+                        }
+                        if(countStars < 3)
+                        {
+                            stars_array.add(movie_star2);
+                            starsId_array.add(movie_starid2);
+                            countStars++;
+                        }
+                    }
+                } while(rs.next());
+
+
+
+                // write JSON string to output
+                out.write(jsonArray.toString());
+                // set response status to 200 (OK)
+                response.setStatus(200);
             }
 
-            response.getWriter().write(responseJsonObject.toString());
-        } catch (Exception e){
+            rs.close();
+            statement.close();
+            dbcon.close();
+
+        } catch (Exception e) {
+
+            // write error message JSON object to output
             JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
             // set reponse status to 500 (Internal Server Error)
             response.setStatus(500);
+
         }
+        out.close();
+
     }
 }
+
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,88 +1,108 @@
-// let search_form = $("#search_form");
-//
-// /**
-//  * Handle the data returned by LoginServlet
-//  * @param resultDataString jsonObject
-//  */
-//
-//
-//
-// function getParameterByName(target) {
-//     // Get request URL
-//     let url = window.location.href;
-//     // Encode target parameter name to url encoding
-//     target = target.replace(/[\[\]]/g, "\\$&");
-//
-//     // Ues regular expression to find matched parameter value
-//     let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
-//         results = regex.exec(url);
-//     if (!results) return null;
-//     if (!results[2]) return '';
-//
-//     // Return the decoded parameter value
-//     return decodeURIComponent(results[2].replace(/\+/g, " "));
-// }
-//
-// function handleSearchResult(resultData) {
-//     let movietitle = resultData[0]["title"];
-//     // window.location.replace("api/movielist?title" + movietitle);
-//     window.location.replace("movielist.html");
-// }
-//
-// /**
-//  * Submit the form content with POST method
-//  * @param formSubmitEvent
-//  */
-// function submitSearchForm(formSubmitEvent) {
-//     console.log("submit search form");
-//     /**
-//      * When users click the submit button, the browser will not direct
-//      * users to the url defined in HTML form. Instead, it will call this
-//      * event handler when the event is triggered.
-//      */
-//     formSubmitEvent.preventDefault();
-//
-//     jQuery.ajax(
-//         /* this will lead us to LoginServlet */
-//         "api/search", {
-//             method: "GET",
-//             // Serialize the login form to the data sent by POST request
-//             data: search_form.serialize(),
-//             success: (resultData) => handleSearchResult(resultData)
-//         }
-//     );
-// }
-//
-//
-//
-// // Bind the submit action of the form to a handler function
-// search_form.submit(submitSearchForm);
-
-
-
 let search_form = $("#search_form");
 
 /**
- * Handle the data returned by LoginServlet
- * @param resultDataString jsonObject
+ * Handle the data returned by SearchServlet
+ * @param resultData jsonObject
  */
-function handleSearchResult(resultDataString) {
-    let resultDataJson = JSON.parse(resultDataString);
-
-    console.log("handle login response");
-    console.log(resultDataJson);
-    console.log(resultDataJson["status"]);
-
-    // If login succeeds, it will redirect the user to index.html
-    if (resultDataJson["status"] === "success") {
-        window.location.replace("movielist.html");
-    } else {
-        // If login fails, the web page will display
-        // error messages on <div> with id "login_error_message"
-        console.log("show error message");
-        console.log(resultDataJson["message"]);
-        $("#search_error_message").text(resultDataJson["message"]);
+function myFunction() {
+    var x = document.getElementById("search_form");
+    if(x.style.display == "block")
+    {
+        x.style.display = "none";
+        exit;
     }
+    x.style.display = "block";
+
+}
+function submitform() {
+    var x = document.getElementById("movie_list_table2");
+    x.style.display = "block";
+
+}
+
+
+function handleSearchResult(resultData) {
+    console.log("handleSearchResult: populating MovieList table from resultData");
+
+    // Populate the star table
+    // Find the empty table body by id "star_table_body"
+    let movieListTableElement2 = $("#movie_list_table_body2");
+
+    movieListTableElement2.html("");
+    // Iterate through resultData, no more than 10 entries
+    if(resultData[0]["searchType"] == "TITLE")
+    {
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML += "<th>" + '<a href="single-movie.html?id=' + resultData[0]['movie_id'] + '">'
+            + resultData[0]["movie_title"] + "</th>";
+        rowHTML += "<th>" + resultData[0]["movie_year"] + "</th>";
+        rowHTML += "<th>" + resultData[0]["movie_director"] + "</th>";
+        rowHTML += "<th>" + resultData[0]["movie_nameOfGenres"] + "</th>";
+        rowHTML += "<th>";
+        for (let i = 0; i < 3; i++) {
+            if (i != 2) {
+                rowHTML += '<a href="single-star.html?id=' + resultData[0]['movie_starid'][i] + '">'
+                    + resultData[0]["movie_star"][i] + ", "
+            } else {
+                rowHTML += '<a href="single-star.html?id=' + resultData[0]['movie_starid'][i] + '">'
+                    + resultData[0]["movie_star"][i]
+            }
+        }
+        rowHTML += "<th>";
+        rowHTML += "<th>" + resultData[0]["movie_rating"] + "</th>";
+        rowHTML += "</tr>";
+        movieListTableElement2.append(rowHTML);
+    }
+    else if (resultData[0]["searchType"] == "YEAR")
+    {
+        for (let i = 0; i < Math.min(20, resultData.length); i++) {
+
+            // Concatenate the html tags with resultData jsonObject
+            let rowHTML = "";
+            rowHTML += "<tr>";
+            // Add a link to single-star.html with id passed with GET url parameter
+            rowHTML +=
+                "<th>" +
+                '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">'
+                + resultData[i]["movie_title"] +
+                '</a>' +
+                "</th>";
+            rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+            rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+            rowHTML += "<th>" + resultData[i]["movie_nameOfGenres"] + "</th>";
+            // add Stars and hrefs
+            rowHTML += "<th>";
+
+            for (let x = 0; x < Math.min(3, resultData[i]['movie_star'].length); x++) {
+                console.log(resultData[i]['movie_star'])
+                if(x + 1 == Math.min(3, resultData[i]['movie_star'].length )){
+                    rowHTML += '<a href="single-star.html?id=' + resultData[i]['movie_starid'][x] + '">'
+                        + resultData[i]['movie_star'][x]  +
+                        '</a>';
+
+                }
+                else
+                {
+                    rowHTML += '<a href="single-star.html?id=' + resultData[i]['movie_starid'][x] + '">'
+                        + resultData[i]['movie_star'][x] +
+                        '</a>' + ", " ;
+                }
+            }
+            rowHTML += "</th>";
+            rowHTML += "<th>" + resultData[i]["movie_rating"] + "</th>";
+            rowHTML += "</tr>";
+
+            // let rowHTML = "";
+            //  rowHTML += "</th>";
+            //  rowHTML += "<th>" + "HI" + "</th>";
+            // rowHTML += "</tr>";
+            // Append the row created to the table body, which will refresh the page
+            movieListTableElement2.append(rowHTML);
+        }
+    }
+
+
 }
 
 /**
@@ -98,13 +118,14 @@ function submitSearchForm(formSubmitEvent) {
      */
     formSubmitEvent.preventDefault();
 
-    $.ajax(
-        /* this will lead us to LoginServlet */
-        "api/search", {
-            method: "POST",
-            // Serialize the login form to the data sent by POST request
-            data: search_form.serialize(),
-            success: handleSearchResult
+    jQuery.ajax(
+      {
+          dataType: "json",
+          method: "GET",
+          // Serialize the login form to the data sent by POST request
+          data: search_form.serialize(),
+          url: "api/search",
+          success: (resultData) => handleSearchResult(resultData)
         }
     );
 }
