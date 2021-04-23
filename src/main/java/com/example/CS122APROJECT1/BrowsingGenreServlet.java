73cfd3a9e02823c25496c017c748db23540f20d9
One-EyedMoon <martinezjacoba2014@gmail.com>
2021-04-22 23:33:38 -0700

    Adding browsing information to website [in-progress]

---
+++ src/main/java/com/example/CS122APROJECT1/BrowsingGenreServlet.java
@@ -0,0 +1,85 @@
+package com.example.CS122APROJECT1;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.Gson;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+@WebServlet(name = "BrowsingGenreServlet", urlPatterns = "/api/browsingGenres")
+public class BrowsingGenreServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.xml
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        try {
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+            // Declare our statement
+            Statement statement = dbcon.createStatement();
+            String query = "SELECT * FROM genres limit 100";
+            // Perform the query
+            ResultSet rs = statement.executeQuery(query);
+            JsonArray jsonArray = new JsonArray();
+            rs.next();
+            while (rs.next()) {
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("name", rs.getString("name"));
+                jsonObject.addProperty("id", rs.getString("id"));
+                jsonArray.add(jsonObject);
+            }
+            // write JSON string to output
+            out.write(jsonArray.toString());
+            // set response status to 200 (OK)
+            response.setStatus(200);
+            rs.close();
+            statement.close();
+            dbcon.close();
+        } catch (Exception e) {
+
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // set reponse status to 500 (Internal Server Error)
+            response.setStatus(500);
+
+        }
+        out.close();
+    }
+}
